52eba15c9ef88d12fb8014d4051579245185ea8c
Michael G. Schwern <schwern@pobox.com>
2010-07-20 20:21:52 -0700

    Very basic Paths docs.

--- lib/Paths.pm
+++ lib/Paths.pm
@@ -8,4 +8,23 @@ use 5.010;
 use File;
 use Dir;
 
+
+=head1 NAME
+
+Paths - Grand Unified File and Directory objects
+
+=head1 SYNOPSIS
+
+    use Paths;
+
+    my $file = File->new("/path/to/some/file");
+    my $dir  = Dir->new("/path/to/some/dir/");
+
+=head1 DESCRIPTION
+
+Paths loads the File and Dir class which encapsulates just about
+everything you might want to do with a file or directory.
+
+=cut
+
 1;
