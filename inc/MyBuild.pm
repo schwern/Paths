59203bd734f5125c220c28cc81b77480191410ce
Michael G. Schwern <schwern@pobox.com>
2010-07-20 18:53:47 -0700

    Skeleton

---
+++ inc/MyBuild.pm
@@ -0,0 +1,38 @@
+package MyBuild;
+
+use 5.010;
+use base 'Module::Build';
+
+
+# Run perltidy over all the Perl code
+# Borrowed from Test::Harness
+sub ACTION_tidy {
+    my $self = shift;
+
+    my %found_files = map { %$_ } $self->find_pm_files,
+      $self->_find_file_by_type( 'pm', 't' ),
+      $self->_find_file_by_type( 'pm', 'inc' ),
+      $self->_find_file_by_type( 't',  't' ),
+      $self->_find_file_by_type( 'PL', '.' );
+
+    my @files = ( keys %found_files, map { $self->localize_file_path($_) } @extra );
+
+
+    print "Running perltidy on @{[ scalar @files ]} files...\n";
+    for my $file ( sort { $a cmp $b } @files ) {
+        print "  $file\n";
+        system( 'perltidy', '-b', $file );
+        unlink("$file.bak") if $? == 0;
+    }
+}
+
+sub ACTION_critic {
+    my $self = shift;
+
+    my @files = keys %{ $self->find_pm_files };
+
+    print "Running perlcritic on @{[ scalar @files ]} files...\n";
+    system( "perlcritic", @files );
+}
+
+1;
